9722d1d664999ab3b69dbbf3625c9632e02c7832
serenkapanoglu <70541836+serenkapanoglu@users.noreply.github.com>
2023-10-20 16:47:22 -0400

    Add files via upload

---
+++ src/Components/TodoList.js
@@ -0,0 +1,37 @@
+import React, {useState} from "react";
+import AddNewTodo from "./AddNewTodo";
+const TodoList = () => {
+   const [todos, setTodos] = useState([
+        {text: 'Pay bills', id:1},
+        {text: 'Do your homework', id:2},
+        {text: 'Feed the dog', id:3},
+    ]);
+
+const [count, setCount] = useState(0);
+
+
+
+    const addTodo = (text) => {
+        setTodos([
+            ...todos,
+            {text, id:Math.random()}
+        ]);
+
+    }
+ return (
+    <div>
+        <ul style = {{listStyleType:'none'}}>
+            {
+                todos.map(todo => {
+                    return(<li key={todo.id}>{todo.text}</li>)
+                })
+            }
+        </ul>
+        <AddNewTodo addTodo={addTodo}/>
+        <button onClick={() => setCount(count +1)}>Score: {count}</button>
+    </div>
+ )
+}
+
+
+export default TodoList;
